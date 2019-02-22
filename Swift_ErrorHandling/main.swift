946ec2140b9297f69190aebfe6b831e124460dd8
MacStudent <macstudent@126-iMac07.local>
2019-02-21 19:01:57 -0500

    Initial Commit

---
+++ Swift_ErrorHandling/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_ErrorHandling
+//
+//  Created by MacStudent on 2019-02-21.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
